367727076ba75e124c2dc6daf4dbb0dcf81fe407
Anuphap <anupharpae@gmail.com>
2015-08-15 11:49:37 +0700

    A-update-2015-8-15

--- app/src/main/java/com/mobcomlab/sciweek/adapter/QuestAdapter.java
+++ app/src/main/java/com/mobcomlab/sciweek/adapter/QuestAdapter.java
@@ -1,20 +1,25 @@
 package com.mobcomlab.sciweek.adapter;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
+import android.os.Build;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mobcomlab.sciweek.MainActivity;
 import com.mobcomlab.sciweek.R;
 
+import com.mobcomlab.sciweek.managers.DatabaseManager;
 import com.mobcomlab.sciweek.models.QuestContents;
 import com.mobcomlab.sciweek.models.QuestModel;
 import com.mobcomlab.sciweek.uiBeacon.BeaconActivity;
@@ -78,6 +83,22 @@ public class QuestAdapter extends RecyclerView.Adapter<QuestAdapter.ViewHolder>{
         } else if (objQuest.getIcon().equals("beacon")) {
             holder.image.setImageResource(completed ? R.drawable.find_clear : R.drawable.find);
         }
+        if(objQuest.getStatus()==3){
+
+            if (objQuest.getIcon().equals("math")) {
+                holder.image.setImageResource(R.drawable.math_problem);
+            } else if (objQuest.getIcon().equals("quest")) {
+                holder.image.setImageResource(R.drawable.qa_problem);
+            } else if (objQuest.getIcon().equals("qr")) {
+                holder.image.setImageResource(R.drawable.qr_problem);
+            } else if (objQuest.getIcon().equals("maze")) {
+                holder.image.setImageResource(R.drawable.maze_problem);
+            } else if (objQuest.getIcon().equals("mobcom")) {
+                holder.image.setImageResource(R.drawable.mcl_problem);
+            } else if (objQuest.getIcon().equals("beacon")) {
+                holder.image.setImageResource(R.drawable.find_problem);
+            }
+        }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,9 +118,33 @@ public class QuestAdapter extends RecyclerView.Adapter<QuestAdapter.ViewHolder>{
 
                 } else if (objQuest.getType() == 3) {
 
-                    Intent intent = new Intent(context, BeaconActivity.class);
-                    intent.putExtra(QuestContents.QUEST_ID, objQuest.getId());
-                    ((Activity) context).startActivityForResult(intent, MainActivity.rg_update);
+                    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2){
+                        Intent intent = new Intent(context, BeaconActivity.class);
+                        intent.putExtra(QuestContents.QUEST_ID, objQuest.getId());
+                        ((Activity) context).startActivityForResult(intent, MainActivity.rg_update);
+
+                    }else {
+                        /*Toast.makeText(context,"555555",Toast.LENGTH_SHORT).show();*/
+                        final Dialog dialog = new Dialog(context);
+                        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                        dialog.setContentView(R.layout.dialog_check);
+
+                        Button btn_save = (Button) dialog.findViewById(R.id.btn_save);
+                        btn_save.setText("ตกลง");
+                        btn_save.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                DatabaseManager databaseManager = new DatabaseManager(context);
+                                databaseManager.UpdateStatus(objQuest.getId(), 3);
+                                dialog.dismiss();
+                                notifyDataSetChanged();
+
+                            }
+                        });
+                        dialog.show();
+
+
+                    }
 
                 }
             }
